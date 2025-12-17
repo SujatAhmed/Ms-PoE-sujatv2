54d0deace9542deea39f6dcff952ade4a414565f
Sujat Ahmed <sujatahmed842002@gmail.com>
2025-12-17 16:11:32 +0600

    reduced the sample run to save compute

--- src/ms_poe.sh
+++ src/ms_poe.sh
@@ -4,7 +4,7 @@ CUDA_VISIBLE_DEVICES=0 python -u inference.py \
     --output_path mdqa_results/ours-vicuna_7b-10doc-answer1-ratio1.2to1.8.jsonl \
     --model_name lmsys/vicuna-7b-v1.5 \
     --seed 42\
-    --sample_num 500 \
+    --sample_num 100 \
     --answer_idx 1 \
     --enable_ms_poe \
     --apply_layers "2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31" \
