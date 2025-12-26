2b864249b9027c81400d7475ae0cbb87de29d7a8
Sujat Ahmed <sujatahmed842002@gmail.com>
2025-12-26 17:27:13 +0600

    testing powerlaw

--- utils/modify_arch/llama.py
+++ utils/modify_arch/llama.py
@@ -89,7 +89,7 @@ class MsPoELlamaRotaryEmbedding(nn.Module):
         self.num_heads = num_heads
 
         # Build here to make `torch.jit.trace` work.
-        self._set_cos_sin_cache_sigmoid(
+        self._set_cos_sin_cache_powerlaw(
             seq_len=max_position_embeddings, device=self.inv_freq.device, dtype=torch.get_default_dtype()
         )
 
@@ -259,8 +259,8 @@ class MsPoELlamaRotaryEmbedding(nn.Module):
     def forward(self, x, seq_len=None):
         # x: [bs, num_attention_heads, seq_len, head_size]
         if seq_len > self.max_seq_len_cached:
-            self._set_cos_sin_cache_sigmoid(seq_len=seq_len, device=x.device, dtype=x.dtype)
-            # self._set_cos_sin_cache_powerlaw(seq_len=seq_len, device=x.device, dtype=x.dtype)
+            # self._set_cos_sin_cache_sigmoid(seq_len=seq_len, device=x.device, dtype=x.dtype)
+             self._set_cos_sin_cache_powerlaw(seq_len=seq_len, device=x.device, dtype=x.dtype)
             # self._set_cos_sin_cache_beta(seq_len=seq_len, device=x.device, dtype=x.dtype)
             # self._set_cos_sin_cache_exponential(seq_len=seq_len, device=x.device, dtype=x.dtype)
             # self._set_cos_sin_cache_softmax(seq_len=seq_len, device=x.device, dtype=x.dtype)
