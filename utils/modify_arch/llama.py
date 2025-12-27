a595b98924b8cb730ddb6037a7eaf1c5a09d2e75
Sujat Ahmed <sujatahmed842002@gmail.com>
2025-12-27 15:53:39 +0600

    testing

--- utils/modify_arch/llama.py
+++ utils/modify_arch/llama.py
@@ -211,7 +211,7 @@ class MsPoELlamaRotaryEmbedding(nn.Module):
         seq_len,
         device,
         dtype,
-        alpha=0.1,   # depth of the valley
+        alpha=0.05,   # depth of the valley
     ):
         """
         Quadratic U-shaped probability:
