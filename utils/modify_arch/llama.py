84fd2b3d45b59392ff6fc47a42dceb51b21ca635
Sujat Ahmed <sujatahmed842002@gmail.com>
2025-12-27 14:04:32 +0600

    changes

--- utils/modify_arch/llama.py
+++ utils/modify_arch/llama.py
@@ -176,7 +176,7 @@ class MsPoELlamaRotaryEmbedding(nn.Module):
         seq_len,
         device,
         dtype,
-        m=10,          # lambda
+        m=25,          # lambda
     ):
         """
         Exponential probability:
